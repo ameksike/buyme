fc0fb94e4bf0d7727ea9add4e06d59b0af97fbd4
Antonio Membrides Espinosa <tonykssa@gmail.com>
2025-01-06 21:16:18 +0100

    add Startup

--- frontend/src/app/(root)/page.tsx
+++ frontend/src/app/(root)/page.tsx
@@ -1,3 +1,4 @@
+import { StartupCard } from "@/components/StartupCard";
 import { SearchForm } from "../../components/SearchForm";
 
 interface Props {
@@ -5,6 +6,19 @@ interface Props {
 }
 export default async function Home({ searchParams }: Props) {
   const query = (await searchParams).query;
+  const { data: posts } = {
+    data: [{
+      _createdAt: new Date(),
+      viewa: 55,
+      author: { _id: 1 },
+      _id: 1,
+      description: "This is a description",
+      image: "https://aigeeked.com/wp-content/uploads/2022/12/ai-image-generator.jpg",
+      category: "Robots",
+      title: "we rebots",
+    }]
+  } // await sanityFetch({ query: STARTUPS_QUERY, params });
+
   return (
     <>
       <section className="pink_container">
@@ -19,6 +33,21 @@ export default async function Home({ searchParams }: Props) {
 
         <SearchForm query={query} />
 
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: any) => ( //StartupTypeCard
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No startups found</p>
+          )}
+        </ul>
       </section>
     </>
   );
